7cd857ba1a609acef7886c2ac0bd506ee6955bf0
Rosebud <rkle408@gmail.com>
2022-10-28 11:23:17 -0700

    fixed bugs, now able to displate JATE, save text on reload

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -30,7 +30,6 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-  console.log("clicked");
     // Clear prompt
     window.deferredPrompt = null;
 });
