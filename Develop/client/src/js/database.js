4655747b997c15c5d37e20149ba598d96497d1ed
Rosebud <rkle408@gmail.com>
2022-10-28 13:51:47 -0700

    minor edits to get full functionality, including 'install' button

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -26,7 +26,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.put({ content });
+  const request = store.put({ id: 1, value: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -34,24 +34,25 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {  console.log('GET from the database');
+export const getDb = async () => { 
+  console.log('GET from the database');
 
-// Create a connection to the database database and version we want to use.
-const jateDb = await openDB('jate', 1);
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
 
-// Create a new transaction and specify the database and data privileges.
-const tx = jateDb.transaction('jate', 'readonly');
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readonly');
 
-// Open up the desired object store.
-const store = tx.objectStore('jate');
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
 
-// Use the .getAll() method to get all data in the database.
-const request = store.getAll();
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
 
-// Get confirmation of the request.
-const result = await request;
-console.log('result.value', result);
-return result.content;
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result.value;
 };
 
 initdb();
